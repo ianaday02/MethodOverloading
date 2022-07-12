5234bb702c4546948839ee50c609b4416b637772
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-12 10:54:51 -0500

    initial commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        int Add(int x, int y)
+        {
+            return (x + y);
+        }
+        decimal Add(decimal x, decimal y)
+        {
+            return (x + y);
+        }
+
+       string Add(int x, int y, bool isTrue)
+        {
+            if (isTrue == true)
+            {
+                if (x + y == 1)
+                {
+                    return ("1 dollar");
+                }
+                else return ($"{x + y} dollars");
+            }
+            else
+            {
+                return ($"{x + y}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
